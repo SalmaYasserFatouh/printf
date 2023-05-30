9ee21e9dcc65934c1e1f051510c87618aa2810dc
Ayahkhattab <ayaahkhattab2017@gmail.com>
2023-05-30 09:11:31 -0700

    functions.c

--- _printf.c
+++ _printf.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
- * @format: format
- * Return: Printed chars
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
@@ -25,7 +24,7 @@ int _printf(const char *format, ...)
 		{
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
-			print_buffer(buffer, &buff_ind);
+				print_buffer(buffer, &buff_ind);
 			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
